42af2773da8a9123c00c505fafffe42cf5ff8e83
Sayantani Mitra <emailsayantani@gmail.com>
2016-02-27 11:32:34 -0600

    Euler_13.py

--- Euler_13.py
+++ Euler_13.py
@@ -2,11 +2,21 @@
 
 
 def add(arr, a, b):
+    """
+
+    :param arr: Array of numbers
+    :param a: Index to start
+    :param b: Index to end
+    :return: Digits in the sum of numbers.
+    """
     add = 0
     for i in range(0, len(arr)):  # range because i is the index. If only 'for in arr' is used, it means i is the
         # value and not the index. And we need index to sum the numbers in the array.
         add += arr[i]
-    print(str(add)[a:b])
+    return str(add)[a:b]
 
 
-print(add([1, 3, 45, 10, 220, 1000, 150], 1, 3))
+def test_euler_13():
+    assert add([1, 3, 45, 10, 220, 1000, 150], 0, 3) == '142'
+    assert add([1, 3, 45, 10, -220, 1000, -150], 0, 3) == '689'
+    assert add([1, 3.9, 45, 10, -220, -1000, -150], 0, 3) == '-13'
