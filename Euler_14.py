198cdb84a08e5c788355016b21feadd804b6c4f7
Sayantani Mitra <emailsayantani@gmail.com>
2016-03-01 23:02:19 -0600

    Documentation and Unittests added

--- Euler_14.py
+++ Euler_14.py
@@ -1,4 +1,4 @@
-'''Longest Collatz sequence:
+''' Longest Collatz sequence:
 The following iterative sequence is defined for the set of positive integers:
 
 n → n/2 (n is even)
@@ -7,21 +7,39 @@ n → 3n + 1 (n is odd)
 Using the rule above and starting with 13, we generate the following sequence:
 
 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
-It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been
+proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
 
 Which starting number, under one million, produces the longest chain?
 
-NOTE: Once the chain starts the terms are allowed to go above one million.'''
+NOTE: Once the chain starts the terms are allowed to go above one million. '''
 
 
 def coll(n):
-    cnt = 1
-    while n > 1:
-        if n%2 ==0:
-            n = n/2
+    """
+
+    :param n: Maximum positive integer till which the Collatz Sequence should be found
+    :return: Count of the chain in the Collatz Sequence
+    """
+    cnt = 1  # Minimum length of the chain
+    while n > 1:  # n will always be a positive integer
+        if n % 2 == 0:
+            n /= 2
         else:
-            n = (3 * n)+1
+            n = (3 * n) + 1
         cnt += 1
     return cnt
 
-print(max(((coll(i),i)) for i in range(0,1000000)))
+
+def max_coll(n):
+    """
+
+    :param n: Maximum positive integer till which the Collatz Sequence should be found
+    :return: Maximum length of the Collatz Sequence and the number itself
+    """
+    return max((coll(n), n) for n in range(0, n))
+
+
+def test_euler_14():
+    assert max_coll(1000000) == (525, 837799)
+    assert max_coll(100000) == (351, 77031)
