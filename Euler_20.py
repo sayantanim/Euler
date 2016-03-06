e4bcc3f3f629798712ba39c80ea52d5921b5ed30
Sayantani Mitra <emailsayantani@gmail.com>
2016-03-06 10:29:01 -0600

    Documentation and Unittests added

--- Euler_20.py
+++ Euler_20.py
@@ -6,13 +6,48 @@ and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
 
 Find the sum of the digits in the number 100!'''
 
-from math import factorial
+'''Version 1:'''
 
-def fact_sum(n):
-    fact = str(factorial(n))
+# from math import factorial
+#
+# def fact_sum(n):
+#     fact = str(factorial(n))
+#     add = 0
+#     for i in range(0, len(fact)):
+#        add += int(fact[i])
+#     return add
+#
+# print(fact_sum(100))
+
+'''Version 2:'''
+
+
+def facto(num):  # Same as in Euler_15.py
+    """
+
+    :param num: Number for which factorial is to be found
+    :return: Factorial of the number
+
+    Note: Factorial is calculated for non-negative integer
+    """
+    prod = 1
+    for i in range(1, num + 1):
+        prod *= i
+    return prod
+
+
+def fact_sum(num):
+    """
+
+    :param num: Number for which factorial sum is to be found
+    :return: Sum of digits of factorial value
+    """
+    fact = str(facto(num))
     add = 0
     for i in range(0, len(fact)):
-       add += int(fact[i])
+        add += int(fact[i])
     return add
 
-print(fact_sum(100))
+
+def test_euler_20():
+    assert fact_sum(100) == 648
